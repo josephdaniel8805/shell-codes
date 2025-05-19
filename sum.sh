70187979f3b31923f8bd03aea297109d2d3cd32e
Joseph Daniel <josephdaniel8805@gmail.com>
2025-05-19 17:59:15 +0530

    To find sum of digits of a number

--- sum.sh
+++ sum.sh
@@ -1,13 +1,13 @@
-echo "SUM OF FIRST N NATURAL NUMBERS"  # Displays a title
-echo "ENTER THE LIMIT N:"              # Prompt user for input
-read n                                 # Read the value of n
-i=1                                    # Initialize counter
-sum=0                                  # Initialize sum
+echo "SUM OF FIRST N NATURAL NUMBERS"  
+echo "ENTER THE LIMIT N:"              
+read n                                 
+i=1                                   
+sum=0                                  
 
-while (( i <= n ))                     # Loop from 1 to n
+while (( i <= n ))                     
 do
-  (( sum = sum + i ))                  # Add i to sum
-  (( i = i + 1 ))                      # Increment i
+  (( sum = sum + i ))                  
+  (( i = i + 1 ))                      
 done
 
-echo "Sum: $sum"                       # Display the result
+echo "Sum: $sum"                       
